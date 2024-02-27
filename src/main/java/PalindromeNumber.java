561d879a3d8907afd1d9972731ca3c44fba23f53
vencazi1 <venkat.hariharan@gmail.com>
2024-02-27 10:57:35 -0800

    Palindrome Number

---
+++ src/main/java/PalindromeNumber.java
@@ -0,0 +1,37 @@
+/**
+ * Input: x = 121
+ * Output: true
+ * Explanation: 121 reads as 121 from left to right and from right to left.
+ * Example 2:
+ *
+ * Input: x = -121
+ * Output: false
+ * Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+ * Example 3:
+ *
+ * Input: x = 10
+ * Output: false
+ * Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+ *
+ *
+ * Constraints:
+ *
+ * -231 <= x <= 231 - 1
+ */
+
+public class PalindromeNumber {
+
+    public boolean isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        }
+        String xStr = String.valueOf(x);
+        String reverseStr = new StringBuilder(xStr).reverse().toString();
+        return xStr.equals(reverseStr);
+    }
+
+    public static void main(String[] args) {
+        PalindromeNumber palindromeNumber = new PalindromeNumber();
+        System.out.println(palindromeNumber.isPalindrome(121));
+    }
+}
